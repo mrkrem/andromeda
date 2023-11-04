f202d04a03bd6c68c7a5a507bb10585c049903a5
ilya <ilya@emall.ru>
2023-11-04 17:26:21 +0300

    доделали игру угадай число

---
+++ for_cycle.py
@@ -0,0 +1,14 @@
+for counter in range(10):
+    print(counter)
+    
+for counter in range(10):
+    print("You are welcome")
+    
+for sandwich in range(1, 6):
+    print(f"Сделал {sandwich}ый бутер")
+    
+for i in range(1, 11):
+    print(i)
+    
+for i in range(100, 201, 100):
+    print(i)
