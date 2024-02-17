1b7f9722b5c53c520770be09f2c2393f4176b429
mrkrem <145851943+mrkrem@users.noreply.github.com>
2024-02-17 21:44:20 +0300

    Update mortal_combat.py

--- mortal_combat.py
+++ mortal_combat.py
@@ -11,12 +11,12 @@ class Fighter:
 fighter1 = Fighter('Скорпион', 'Скорпионья супер атака')
 fighter2 = Fighter('Охлодулькин', 'Сосульки-Ударюльки')
 fighter3 = Fighter('Кунг-фу панда', 'Скидыщ')
-fighter4 = Fighter('Мария Михайловна', 'Удар в виде подфакториального субфакториала 30 в синусе 5 ')
-fighter5 = Fighter('Галина Вевчяславовна', 'Удар переменным током')
-fighter6 = Fighter('Юлия Фёдоровна', 'Деепричастный оборот')
-fighter7 = Fighter('Ирина Олеговна', 'Удар циркулем за неправильное построение тригонометрии')
-fighter8 = Fighter('Эльфар Зуфарович', 'Удар бояром с ножом')
-fighter9 = Fighter('Чулпан Рамизовна', 'LONDON OF THE CAPITELE OF GRATE BTITAN')
+fighter4 = Fighter('Учитель математики', 'Удар в виде подфакториального субфакториала 30 в синусе 5 ')
+fighter5 = Fighter('Учитель физики', 'Удар переменным током')
+fighter6 = Fighter('Учитель русского языка и литературы', 'Деепричастный оборот')
+fighter7 = Fighter('Учитель ИЗО', 'Удар циркулем за неправильное построение тригонометрии')
+fighter8 = Fighter('Учитель истории', 'Удар бояром с ножом')
+fighter9 = Fighter('Учитель английского языка', 'LONDON OF THE CAPITELE OF GRATE BTITAN')
 
 fighters = [fighter1, fighter2, fighter3, fighter4, fighter5, fighter6, fighter7, fighter8, fighter9]
 enemy1 = random.choice(fighters)
