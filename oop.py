42ac16a3066355fa2510c7d29df422c27ebf2b02
ilya <ilya@emall.ru>
2024-02-17 17:27:31 +0300

    mortal-combat

--- oop.py
+++ oop.py
@@ -36,3 +36,56 @@ class Human():
 
 human = Human('Вася', 'Пупкин',4,5,30)
 human.happy_birthday()
+
+
+
+class PhrasweDecoration:
+    def decorate(self, phrase):
+        line = '-' * (len(phrase)+4)
+        print(line)
+        print('|', phrase, '|')
+        print(line)
+        
+        
+text = PhrasweDecoration()
+text.decorate('Я люблю ООП')
+
+
+class Father:
+    def __init__(self, name:str, age:int):
+        self.name = name.capitalize()
+        self.age = age
+        self.gender = 'мужчина'
+        self.power = 0
+        
+    def swim(self):
+        print(f'{self.name} плавает брассом')
+        
+    def dispute(self):
+        print(f'{self.name} спорит басом')
+        
+    def chopping(self):
+        self.power += 1
+        print(f'{self.name} рубит дрова. Сила папы = {self.power}')
+        
+father = Father(name='Олег', age=55)
+father.swim()
+father.dispute()
+father.chopping()
+ 
+class Son(Father):
+    def __init__(self, name: str, age: int, sport:int):
+        super().__init__(name, age)
+        self.sport = sport    
+    
+    def dispute(self):
+        super().dispute()
+        print(f'{self.name} спорит')
+        
+
+son = Son('Роман', 5, 23)
+son.swim()
+son.dispute()
+son.chopping()
+        
+        
