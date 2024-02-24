7d706a729c7b2c0c6259be45e358c670c21cc6a5
ilya <ilya@emall.ru>
2024-02-24 17:51:40 +0300

    mortal combat

--- mortal_combat.py
+++ mortal_combat.py
@@ -6,6 +6,32 @@ class Fighter:
         self.power = random.randint(1, 5)
         self.hp = 100
         self.nam_fat = name_fatality
+        self.fat_switch = True
+        
+    def attack(self, attacked_fighter):
+        if self.fat_switch:
+            self.fatality(attacked_fighter)
+        attacked_fighter.hp -= self.power
+        print(f'{self.name} наносит {self.power} ед. урона персонажу {attacked_fighter.name}')
+        self.harakiri()
+        
+            
+    def say_info(self):
+        print(f'У персонажа {self.name} {self.hp} ед здоровья, {self.power} единиц силы')
+        
+    def fatality(self, attacked_fighter):
+        if self.hp < 15:
+            damage = random.randint(0, attacked_fighter.hp)
+            attacked_fighter.hp -= damage
+            print(f'{self.name} принимает суператаку {self.nam_fat} {self.name} и наносит {damage} ед. урона персонажу {attacked_fighter.name}')
+            self.fat_switch = False
+            
+            
+    def harakiri(self):
+        if self.hp < 5 and random.randint(1, 2)==1:
+            print(f'{self.name} совершил харакири')
+            self.hp = 0
+            
 
 
 fighter1 = Fighter('Скорпион', 'Скорпионья супер атака')
@@ -23,6 +49,18 @@ enemy1 = random.choice(fighters)
 fighters.remove(enemy1)
 enemy2 = random.choice(fighters)
 fighters.append(enemy1)
-
+    
 
 print(f'{enemy1.name} VS {enemy2.name}')
+
+while True:
+    enemy1.attack(enemy2)
+    enemy2.attack(enemy1)
+    enemy1.say_info()
+    enemy2.say_info()
+    if enemy1.hp <= 0 and enemy2.hp <= 0:
+        print('Ничья')
+        break
+    elif enemy2.hp <= 0:
+        print(f'{enemy1.name} победил')
+        break
