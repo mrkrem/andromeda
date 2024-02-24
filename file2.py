b5e540a2b2bb09750c8cb5654bc21d62140190ee
ilya <ilya@emall.ru>
2024-02-24 17:28:32 +0300

    Консольный Мортал Комбат

--- file2.py
+++
@@ -1,7 +0,0 @@
-import file1
-from file1 import pause, play
-
-file1.play()
-file1.play()
-play()
-pause()
--- modul/pay.py
+++
@@ -1,9 +0,0 @@
-from cost import *
-import random
-
-def pay():
-    money = random.choice(['да','нет'])
-    if money == 'да':
-        print('Вы оплатили ваш заказ')
-    else:
-        print('Вам не хватило денег')
